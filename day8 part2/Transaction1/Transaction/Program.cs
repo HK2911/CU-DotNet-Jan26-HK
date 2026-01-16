3ff7e23571297048daa7ba352281cfc512117887
HK2911 <hk1295217@gmail.com>
2026-01-16 10:44:25 +0530

    day8 part2

---
+++ day8 part2/Transaction1/Transaction/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Transaction
+{
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                
+                string input = Console.ReadLine();
+
+                string[] parts = input.Split('#');
+
+                string transactionId = parts[0];
+                string accountHolder = parts[1];
+                string narration = parts[2];
+
+             
+                narration = narration.Trim();
+
+                while (narration.Contains("  "))
+                {
+                    narration = narration.Replace("  ", " ");
+                }
+
+                narration = narration.ToLower();
+
+                bool hasDeposit = narration.Contains("deposit");
+                bool hasWithdrawal = narration.Contains("withdrawal");
+                bool hasTransfer = narration.Contains("transfer");
+
+                bool hasKeyword = hasDeposit || hasWithdrawal || hasTransfer;
+
+                string standardNarration = "cash deposit successful";
+                bool isStandard = narration.Equals(standardNarration);
+
+                string category;
+
+                if (!hasKeyword)
+                {
+                    category = "NON-FINANCIAL TRANSACTION";
+                }
+                else if (hasKeyword && isStandard)
+                {
+                    category = "STANDARD TRANSACTION";
+                }
+                else
+                {
+                    category = "CUSTOM TRANSACTION";
+                }
+
+                Console.WriteLine($"{"Transaction ID".PadRight(15)}: {transactionId}");
+                Console.WriteLine($"{"Account Holder".PadRight(15)}: {accountHolder}");
+                Console.WriteLine($"{"Narration".PadRight(15)}: {narration}");
+                Console.WriteLine($"{"Category".PadRight(15)}: {category}");
+            }
+        }
+}
