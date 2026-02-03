73cf34fc316d44d764e0efe75c83fa4d5ae4c580
HK2911 <hk1295217@gmail.com>
2026-02-03 14:36:15 +0530

    Day24 exercise1

---
+++ Day24_Exer/Day24_Exer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+Hashtable ht = new Hashtable()
+            {
+                { 101, "Alice" },
+                { 102, "Bob" },
+                { 103, "Charlie" },
+                { 104, "Diana" }
+           };
+
+
+if (!ht.ContainsKey(105))
+{
+    ht.Add(105, "Edward");
+}
+else
+{
+    Console.WriteLine("ID already exists");
+}
+
+string employeeName = (string)ht[102];
+
+Console.WriteLine(employeeName);
+
+foreach (DictionaryEntry v in ht)
+{
+    Console.WriteLine($"{v.Key} : {v.Value}" );
+}
+
+
+ht.Remove(103);
+
+foreach (DictionaryEntry v in ht)
+{
+    Console.WriteLine($"{v.Key} : {v.Value}");
+}
+
