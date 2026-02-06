b39d31dc1d4b91982df6382f444096c3b148e032
HK2911 <hk1295217@gmail.com>
2026-02-06 14:04:52 +0530

    day27_exercise1

---
+++ Day27_Exercise1/Day27/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStreamDemo
+{
+    internal class Class1
+    {
+        static void Main()
+        {
+            string file = @"../../../file1.txt";
+            Console.WriteLine("Write your content: ");
+            string input = Console.ReadLine();
+
+            using (StreamWriter sw = new StreamWriter(file, true))
+            {
+                sw.WriteLine(input);
+                sw.WriteLine();
+            }
+
+            Console.WriteLine("Your reflection have been saved");
+
+        }
+
+
+    }
+}
