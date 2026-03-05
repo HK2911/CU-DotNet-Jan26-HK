24447ca76b4af37c09f3c8e79e2b1fd73a084a15
HK2911 <hk1295217@gmail.com>
2026-03-05 09:40:34 +0530

    day47

---
+++ Day47_Exercise1/Day47_Exercise1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day47_Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
